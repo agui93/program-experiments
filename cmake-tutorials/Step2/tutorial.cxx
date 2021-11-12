d729171f440db38871389d829edb1bcc06b3fe00
agui93 <agui93@foxmail.com>
2021-11-12 18:47:23 +0800

    update

--- cmake-tutorials/Step2/tutorial.cxx
+++ cmake-tutorials/Step2/tutorial.cxx
@@ -5,22 +5,21 @@
 
 #include "TutorialConfig.h"
 
-int main(int argc, char* argv[])
-{
-  if (argc < 2) {
-    // report version
-    std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
-              << Tutorial_VERSION_MINOR << std::endl;
-    std::cout << "Usage: " << argv[0] << " number" << std::endl;
-    return 1;
-  }
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        // report version
+        std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
+                  << Tutorial_VERSION_MINOR << std::endl;
+        std::cout << "Usage: " << argv[0] << " number" << std::endl;
+        return 1;
+    }
 
-  // convert input to double
-  const double inputValue = std::stod(argv[1]);
+    // convert input to double
+    const double inputValue = std::stod(argv[1]);
 
-  // calculate square root
-  const double outputValue = sqrt(inputValue);
-  std::cout << "The square root of " << inputValue << " is " << outputValue
-            << std::endl;
-  return 0;
+    // calculate square root
+    const double outputValue = sqrt(inputValue);
+    std::cout << "The square root of " << inputValue << " is " << outputValue
+              << std::endl;
+    return 0;
 }
