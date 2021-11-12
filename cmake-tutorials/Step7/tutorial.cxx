ffb6f42ba6b0d6ab6cd4c4c3a3a5658dc49e59e2
agui93 <agui93@foxmail.com>
2021-11-12 15:04:44 +0800

    add cmake tutorials

--- cmake-tutorials/Step7/tutorial.cxx
+++ cmake-tutorials/Step7/tutorial.cxx
@@ -10,27 +10,26 @@
 #  include "MathFunctions.h"
 #endif
 
-int main(int argc, char* argv[])
-{
-  if (argc < 2) {
-    // report version
-    std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
-              << Tutorial_VERSION_MINOR << std::endl;
-    std::cout << "Usage: " << argv[0] << " number" << std::endl;
-    return 1;
-  }
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        // report version
+        std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
+                  << Tutorial_VERSION_MINOR << std::endl;
+        std::cout << "Usage: " << argv[0] << " number" << std::endl;
+        return 1;
+    }
 
-  // convert input to double
-  const double inputValue = std::stod(argv[1]);
+    // convert input to double
+    const double inputValue = std::stod(argv[1]);
 
-  // which square root function should we use?
+    // which square root function should we use?
 #ifdef USE_MYMATH
-  const double outputValue = mysqrt(inputValue);
+    const double outputValue = mysqrt(inputValue);
 #else
-  const double outputValue = sqrt(inputValue);
+    const double outputValue = sqrt(inputValue);
 #endif
 
-  std::cout << "The square root of " << inputValue << " is " << outputValue
-            << std::endl;
-  return 0;
+    std::cout << "The square root of " << inputValue << " is " << outputValue
+              << std::endl;
+    return 0;
 }
